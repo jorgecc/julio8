312b1d292e0c09d5ea51d48f0f2c3063c36b995c
Jorge Castro <jorge.castro.c@gmail.com>
2019-07-08 21:41:28 -0400

    ok.

--- WebForm/WebForm1.aspx.cs
+++ WebForm/WebForm1.aspx.cs
@@ -41,7 +41,9 @@ namespace WebForm
 
                     // cargar el combobox
                     DropDownList1.DataSource = CoffeTypeDal.ListarTodo();
-                    DropDownList1.DataBind(); 
+                    DropDownList1.DataBind();
+                    DropDownList1.SelectedValue=filtro.ToString();
+
                     // cargar la grilla usando ese filtro
                     if (filtro==0) { 
                         GridView1.DataSource = CoffeeDal.ListarTodo(pag);
